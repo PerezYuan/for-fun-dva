dfed7e0dccb893a70784667cf850d080c6bdc212
沐自 <muzi.yzm@alibaba-inc.com>
2018-07-18 17:57:08 +0800

    [+] support https

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 export const PAGE_SIZE = 10;
 
-export const API_LOCATION = "http://api.weijianlife.com";
+export const API_LOCATION = "https://api.weijianlife.com";
 
 // export const API_LOCATION = "http://localhost:3000";
